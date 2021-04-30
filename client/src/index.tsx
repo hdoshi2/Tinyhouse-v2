d9ba24c07ac7ab66983cd7d4f81dc1ba26602046
Hari.Doshi <hdoshi2@gmail.com>
2021-04-30 11:33:16 -0600

    ADD react Listings component.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {render} from 'react-dom';
 import reportWebVitals from './reportWebVitals';
+import {Listings} from './sections';
 
 render(
   <React.StrictMode>
-    <div>Hello World!</div>
+    <Listings title="TinyHouse Listings"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ client/src/sections/Listings/Listings.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+interface Props {
+    title: String;
+}
+
+
+export const Listings = ({title}: Props) => {
+    return <h2>{title}</h2>
+}
+
+
