a67313ddecda8bc871c435295de63267109118e9
Hari.Doshi <hdoshi2@gmail.com>
2021-04-30 22:29:28 -0600

    ADD new fetchListings method

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,4 +1,19 @@
 import React from 'react';
+import {server} from "../../lib/api"
+
+const LISTINGS = `
+query Listings{
+    listings{
+        id
+        title
+        image
+        address
+        price
+        numOfGuests
+        numOfBeds
+        rating
+    }
+}`;
 
 interface Props {
     title: String;
@@ -6,7 +21,15 @@ interface Props {
 
 
 export const Listings = ({title}: Props) => {
-    return <h2>{title}</h2>
+    const fetchListings = async () => {
+        const listings = await server.fetch({query: LISTINGS});
+        console.log(listings.data.listings);
+    }
+
+    return <div>
+            <h2>{title}</h2>
+            <button onClick={fetchListings}>Query Listings!</button>
+        </div>
 }
 
 
