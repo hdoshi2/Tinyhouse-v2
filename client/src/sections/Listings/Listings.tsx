bd60a84ac38edd32d11c3a8f2696515a66f189f3
Hari.Doshi <hdoshi2@gmail.com>
2021-05-01 00:10:44 -0600

    ADD deleteListing method

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {server} from "../../lib/api"
-import {ListingsData} from './types';
+import {ListingsData, DeleteListingVariables, DeleteListingData} from './types';
 
 const LISTINGS = `
 query Listings{
@@ -16,22 +16,50 @@ query Listings{
     }
 }`;
 
-interface Props {
-    title: String;
-}
-
-
-export const Listings = ({title}: Props) => {
-
-    const fetchListings = async () => {
-        const {data} = await server.fetch<ListingsData>({query: LISTINGS});
-        console.log(data);
+const DELETE_LISTING = `
+  mutation DeleteListing($id: ID!) {
+    deleteListing(id: $id) {
+      id
     }
+  }
+`;
 
-    return <div>
-            <h2>{title}</h2>
-            <button onClick={fetchListings}>Query Listings!</button>
-        </div>
+interface Props {
+  title: string;
 }
+
+export const Listings = ({ title }: Props) => {
+
+  const fetchListings = async () => {
+    const { data } = await server.fetch<ListingsData>({ query: LISTINGS });
+    console.log(data); // check the console to see the listings data from our GraphQL Request!
+  };
+
+  const deleteListing = async () => {
+    const { data } = await server.fetch<
+      DeleteListingData,
+      DeleteListingVariables
+    >({
+      query: DELETE_LISTING,
+      variables: {
+        id: "608612e131b6af723a3f004a" // hardcoded id variable,
+      }
+    });
+
+    console.log(data); // check the console to see the result of the mutation!
+  };
+
+
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      <button onClick={fetchListings}>Query Listings!</button>
+      <button onClick={deleteListing}>Delete a listing!</button>
+    </div>
+  );
+
+}
+
 
 
